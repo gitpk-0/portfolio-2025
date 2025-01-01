f30f5eb8c72d5cf9a46409918dce5fe7607021ea
gitpk-0 <kell108p@protonmail.com>
2025-01-01 17:07:39 -0500

    app & shared structured

---
+++ src/app/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<main>
+  <div></div>
+
+  <div class="card"></div>
+
+  <p>
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
+  </p>
+</main>
+
+<style>
+</style>
